c31fedc0e4d41dd2ab067b975ca47e55b3c5acb5
202407030LHH <super8114@yuhan.ac.kr>
2026-02-20 16:56:17 +0900

    리눅스 데이터 코드 수정 필요

--- 00. Study/02. apply/Linux_DB.py
+++ 00. Study/02. apply/Linux_DB.py
@@ -6,18 +6,9 @@ import pandas as pd
 # 공식 페이지에서 작업이 어려워서  인비원 사이트 참조
 linux_data = []
 
-linux_response = requests.get("https://www.invione.com/board/lic/220")
+linux_response = requests.get("https://www.ihd.or.kr/guidecert1.do")
 html = linux_response.text
 soup = BeautifulSoup(html, "html.parser")
 
-linux_items = soup.select("div.lic_date > h4")
-
-for item in linux_items:
-    division = item.select_one(".lic_h4")
-    # written_form = item.select_one("lic_dd:nth-child(1)")
-    # written_exam = item.select_one("lic_dd:nth-child(2)")
-    # written_result_date = item.select_one("lic_dd:nth-child(3)")
-    # practical_form = item.select_one("lic_dd:nth-child(4)")
-    # practical_exam = item.select_one("lic_dd:nth-child(5)")
-    print(division)
-
+category = soup.select_one("h3")
+print(category)    
